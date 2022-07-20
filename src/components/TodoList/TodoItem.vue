8256e2f454d9dccf2703656edecfdfb1863c2622
hellsontime <baber492@gmail.com>
2022-07-20 18:47:39 +0300

    Implement TodoItem component and styles with props

---
+++ src/components/TodoList/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center w-full">
+    <input
+      type="checkbox"
+      class="h-5 w-5 mr-4 focus:outline-none rounded-sm"
+      :id="'todo' + todo.id"
+      :checked="todo.done ? true : false"
+    />
+    <label class="text-lg flex-2" :for="'todo' + todo.id">
+      {{ todo.content }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      required: true,
+      type: Object,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
