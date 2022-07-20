f225bc4df34894803dfe373957a3355c5295c003
hellsontime <baber492@gmail.com>
2022-07-20 18:45:02 +0300

    implement Todo view component and styles

---
+++ src/views/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="max-w-screen-sm mx-auto w-full flex flex-col justify-center p-8 my-20 bg-light-grey rounded-md shadow-md"
+  >
+    <div
+      class="capitalize text-2xl text-at-blue font-bold flex justify-center mb-6"
+    >
+      vue todo app
+    </div>
+
+    <TodoForm />
+
+    <hr />
+
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoForm from "@/components/TodoForm";
+import TodoList from "@/components/TodoList";
+
+export default {
+  components: {
+    TodoForm,
+    TodoList,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
