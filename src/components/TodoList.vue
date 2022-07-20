77ac1c8a64b983237878a4dfbadadfbe4e68ebac
hellsontime <baber492@gmail.com>
2022-07-20 18:47:12 +0300

    Implement TodoList component and styles to map existing todos

---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="w-full flex flex-col mt-6"
+    v-for="(todo, index) in todos.todoList"
+    :key="index"
+  >
+    <TodoItem :todo="todo" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import todoList from "@/constants/todos";
+import TodoItem from "@/components/TodoList/TodoItem";
+
+export default {
+  components: {
+    TodoItem,
+  },
+  setup() {
+    const todos = ref(todoList);
+
+    return { todos };
+  },
+};
+</script>
