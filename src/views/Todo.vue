7fbf35874d366ac5565a8b949d463f47c76af899
hellsontime <baber492@gmail.com>
2022-07-22 17:24:17 +0300

    refactor logo layout

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,11 +1,12 @@
 <template>
   <div
-    class="max-w-screen-md mx-auto w-full flex flex-col justify-center p-8 my-20 bg-light-grey rounded-md shadow-md"
+    class="max-w-screen-md mx-auto w-full flex flex-col justify-center p-8 my-20 bg-light-grey rounded-xl shadow-md"
   >
     <div
-      class="capitalize text-2xl text-at-blue font-bold flex justify-center mb-6"
+      class="uppercase text-2xl text-at-blue font-bold flex justify-start items-center mb-6"
     >
-      vue todo app
+      <fa icon="fa-solid fa-list-check" class="mr-3" />
+      <p class="tracking-wider text-xl">vue todo app</p>
     </div>
 
     <TodoForm />
