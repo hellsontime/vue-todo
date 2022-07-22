3e5c0f11a780d70c47561c0da670b809b6a6e3ab
hellsontime <baber492@gmail.com>
2022-07-22 16:04:28 +0300

    initial todos constant as the test data

---
+++ src/constants/initTodosList.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    userId: 1,
+    content: "lorem lorem lorem lorem lorem lorem lorem ",
+    date: null,
+    done: true,
+  },
+  {
+    id: 2,
+    userId: 1,
+    content: "lorem lorem lorem lorem ",
+    date: null,
+    done: false,
+  },
+  {
+    id: 3,
+    userId: 1,
+    content: "lorem lorem lorem lorem lorem lorem lorem lorem ",
+    done: false,
+    date: "2022-07-22",
+  },
+];
