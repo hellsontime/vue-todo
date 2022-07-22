2ca408630cfd10e3e760abad1901857a42b1e3a2
hellsontime <baber492@gmail.com>
2022-07-22 16:05:14 +0300

    TodoForm component implementation and logic

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,16 +1,31 @@
 <template>
   <form @submit.prevent="addTodo" class="mb-8 relative">
     <input
-      v-model="newTodo"
+      v-model="newTodo.content"
       id="newTodo"
       type="text"
-      class="w-full py-4 px-5 pr-20 focus:outline-none shadow-sm"
+      class="w-full py-4 px-5 pr-24 focus:outline-none shadow-sm rounded-md"
       placeholder="Add new ..."
     />
-    <i class="fa-regular fa-calendar-days"></i>
+    <fa
+      @click="toggleDatepicker"
+      icon="fa-solid fa-calendar-days"
+      class="absolute text-xl font-light top-4 right-[4.5rem] cursor-pointer"
+      :class="dateVisible ? 'text-at-blue' : 'text-grey'"
+    />
+    <Datepicker
+      v-if="dateVisible"
+      v-model="date"
+      format="dd-MM-yyyy"
+      type="date"
+      :minDate="new Date()"
+      class="mt-3 shadow-sm border-none"
+    ></Datepicker>
     <button
+      :disabled="!inputEnabled"
       type="submit"
-      class="bg-at-blue text-white py-1.5 px-2 rounded absolute right-3 top-2.5"
+      class="text-white py-1.5 px-2 rounded absolute right-3 top-2.5"
+      :class="inputEnabled ? 'bg-at-blue' : 'bg-grey'"
     >
       Add
     </button>
@@ -18,18 +33,65 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, watchEffect, computed } from "vue";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import emptyTodo from "@/constants/emptyTodo";
+import store from "@/store";
 
 export default {
+  components: { Datepicker },
   setup() {
-    const newTodo = ref("");
+    // init value for new Todo
+    const newTodo = ref(emptyTodo);
 
-    const addTodo = () => {
-      console.log(newTodo.value);
-      newTodo.value = "";
+    // Add button enabled/disabled tracker
+    const inputEnabled = ref(false);
+    watchEffect(() => {
+      computed(
+        newTodo.value.content === null || newTodo.value.content === ""
+          ? (inputEnabled.value = false)
+          : (inputEnabled.value = true)
+      );
+    });
+
+    // init value for datepicker
+    const date = ref(new Date());
+    const dateVisible = ref(false);
+    const toggleDatepicker = () => {
+      dateVisible.value = !dateVisible.value;
     };
 
-    return { newTodo, addTodo };
+    // clear new todo values after add
+    const clearNewTodo = () => {
+      newTodo.value.content = "";
+      newTodo.value.date = null;
+    };
+
+    // add new todo method
+    const addTodo = () => {
+      if (dateVisible.value) {
+        newTodo.value.date = date.value.toISOString().split("T")[0];
+      }
+      store.commit("add", {
+        userId: newTodo.value.userId,
+        content: newTodo.value.content,
+        date: newTodo.value.date,
+      });
+
+      date.value = new Date();
+      clearNewTodo();
+      dateVisible.value = false;
+    };
+
+    return {
+      newTodo,
+      inputEnabled,
+      addTodo,
+      date,
+      dateVisible,
+      toggleDatepicker,
+    };
   },
 };
 </script>
