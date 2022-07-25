1374f02a080112d84febfa9ef652d485c0cba468
hellsontime <baber492@gmail.com>
2022-07-25 19:40:27 +0300

    TodoForm refactor to work with custom trigger + display current choosed date

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,26 +1,35 @@
 <template>
-  <form @submit.prevent="addTodo" class="mb-6 relative">
+  <form @submit.prevent="addTodo" class="mb-2 relative">
     <input
       v-model="newTodo.content"
       id="newTodo"
       type="text"
-      class="w-full py-4 px-5 pr-24 focus:outline-none shadow-sm rounded-md"
+      class="w-full py-4 px-5 pr-24 focus:outline-none shadow-sm rounded-md relative"
       placeholder="Add new ..."
     />
-    <fa
-      @click="toggleDatepicker"
-      icon="fa-solid fa-calendar-days"
-      class="absolute text-xl font-light top-4 right-[4.5rem] cursor-pointer"
-      :class="dateVisible ? 'text-at-blue' : 'text-grey'"
-    />
+    <div v-if="date" class="absolute top-0 right-[105px] top-[16px] z-10">
+      {{ formatDate }}
+    </div>
+
     <Datepicker
-      v-if="dateVisible"
       v-model="date"
-      format="dd-MM-yyyy"
+      position="right"
+      ref="datepicker"
       type="date"
+      :enableTimePicker="false"
       :minDate="new Date()"
-      class="mt-3 shadow-sm border-none"
-    ></Datepicker>
+      class="mt-3 shadow-sm border-none w-50"
+    >
+      <template #trigger>
+        <fa
+          @click="toggleDatepicker"
+          icon="fa-solid fa-calendar-days"
+          class="absolute text-xl font-light top-4 right-[4.5rem] cursor-pointer"
+          :class="dateVisible ? 'text-at-blue' : 'text-grey'"
+        />
+      </template>
+    </Datepicker>
+
     <button
       :disabled="!inputEnabled"
       type="submit"
@@ -37,6 +46,7 @@ import { ref, watchEffect, computed } from "vue";
 import Datepicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
 import emptyTodo from "@/constants/emptyTodo";
+import { timeConverter } from "@/helpers/dateTimeHelper";
 import store from "@/store";
 
 export default {
@@ -55,12 +65,37 @@ export default {
       );
     });
 
-    // init value for datepicker
-    const date = ref(new Date());
+    // init values for datepicker
+    const date = ref(null);
+    const formatDate = ref(null);
     const dateVisible = ref(false);
+    const datepicker = ref(null);
+
     const toggleDatepicker = () => {
-      dateVisible.value = !dateVisible.value;
+      if (date.value) {
+        date.value = null;
+        datepicker.value.openMenu();
+      }
     };
+
+    watchEffect(() => {
+      if (date.value) {
+        dateVisible.value = true;
+        return;
+      }
+      dateVisible.value = false;
+      return;
+    });
+
+    watchEffect(() => {
+      date.value
+        ? (formatDate.value = timeConverter(date.value.toISOString()))
+        : (formatDate.value = null);
+    });
+
+    watchEffect(() => {
+      date.value ? console.log(date.value.toISOString()) : null;
+    });
 
     // clear new todo values after add
     const clearNewTodo = () => {
@@ -71,7 +106,7 @@ export default {
     // add new todo method
     const addTodo = () => {
       if (dateVisible.value) {
-        newTodo.value.date = date.value.toISOString().split("T")[0];
+        newTodo.value.date = date.value.toISOString();
       }
       store.commit("add", {
         userId: newTodo.value.userId,
@@ -79,7 +114,7 @@ export default {
         date: newTodo.value.date,
       });
 
-      date.value = new Date();
+      date.value = null;
       clearNewTodo();
       dateVisible.value = false;
     };
@@ -89,7 +124,9 @@ export default {
       inputEnabled,
       addTodo,
       date,
+      formatDate,
       dateVisible,
+      datepicker,
       toggleDatepicker,
     };
   },
