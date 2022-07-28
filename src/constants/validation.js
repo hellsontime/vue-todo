704353328511e0caeea1a2afd308466895a6e82b
hellsontime <baber492@gmail.com>
2022-07-28 16:22:31 +0300

    implement validation regex rules

---
+++ src/constants/validation.js
@@ -0,0 +1,17 @@
+const validation = {
+  name: {
+    rule: /^[a-zA-Z]{2,255}$/,
+    message: "Name must contain only letters",
+  },
+  email: {
+    rule: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+    message: "Email is invalid",
+  },
+  password: {
+    rule: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    message:
+      "Password must include minimum 8 characters, at least one letter and one number",
+  },
+};
+
+export default validation;
