f852bcf4b0bfddb7fd723f1679dc72d6009e9a45
hellsontime <baber492@gmail.com>
2022-07-28 14:05:19 +0300

    implement PageNotFound view layout

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="max-w-screen-sm mx-auto w-full flex flex-col justify-center p-8 my-20 bg-light-grey rounded-xl shadow-md"
+  >
+    <h1 class="text-2xl font-bold mb-2">404 - Page Not Found</h1>
+    <p>This page no longer exists or was moved to another location.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
