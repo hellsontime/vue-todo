056f6ef5c4f4718860144cee4efbc5c22f5bc8f9
hellsontime <baber492@gmail.com>
2022-08-01 16:31:22 +0300

    impelemnt card info with scoped slots

---
+++ src/components/CardInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col items-center justify-center my-4">
+    <fa v-if="icon" :icon="icon" class="text-grey text-[48px] mb-4" />
+    <div class="capitalize text-lg text-light-black">
+      <slot name="heading"></slot>
+    </div>
+    <div class="capitalize text-sm text-light-black">
+      <slot name="info"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
