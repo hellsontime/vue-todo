d3e6bc3256f6c0a67d850e4296684f27bc728715
hellsontime <baber492@gmail.com>
2022-08-01 12:53:31 +0300

    TodoError component implementation

---
+++ src/components/TodoError.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col items-center justify-center my-4">
+    <fa
+      icon="fa-solid fa-circle-exclamation"
+      class="text-grey text-[48px] mb-4"
+    />
+    <div class="capitalize text-lg text-light-black">
+      service is unavailable now
+    </div>
+    <div class="capitalize text-sm text-light-black">try again later</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
