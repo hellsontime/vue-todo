abf2546a2fa5b5e97ec03a7866826eb59fc83a47
hellsontime <baber492@gmail.com>
2022-08-01 16:32:50 +0300

    refactor TodoList to use  CradInfo + separate todos logic

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,49 +1,61 @@
 <template>
-  <div v-if="!loading">
-    <hr />
+  <hr />
 
-    <TodoEmpty v-if="!todos.length" />
+  <CardInfo
+    v-if="!storeTodos.length"
+    :icon="'fa-solid fa-hurricane'"
+    class="mt-[40px]"
+  >
+    <template #heading>you have no todos</template>
+    <template #info>let's add some</template>
+  </CardInfo>
 
-    <div v-else>
-      <TodoFilter :updateTodos="updateTodos" />
+  <div v-else>
+    <TodoFilter :updateTodos="updateTodos" />
 
-      <div
-        class="w-full flex flex-col mt-6"
-        v-for="(todo, index) in todos"
-        :key="index"
-      >
-        <TodoItem :todo="todo" />
-      </div>
+    <CardInfo
+      v-if="!todos.length"
+      :icon="'fa-solid fa-filter-circle-xmark'"
+      class="my-[80px]"
+    >
+      <template #heading>no todos fot this filter</template>
+    </CardInfo>
+
+    <div
+      v-else
+      class="w-full flex flex-col mt-6"
+      v-for="(todo, index) in todos"
+      :key="index"
+    >
+      <TodoItem :todo="todo" />
     </div>
   </div>
 </template>
 
 <script>
-import { ref, computed } from "vue";
-import TodoEmpty from "@/components/TodoEmpty";
+import { computed } from "vue";
+import CardInfo from "@/components/CardInfo";
 import TodoItem from "@/components/TodoList/TodoItem";
 import TodoFilter from "@/components/TodoList/TodoFilter";
 import store from "@/store";
 
 export default {
   props: {
-    loading: {
-      required: true,
-      type: Boolean,
+    todos: {
+      type: Array,
+    },
+    updateTodos: {
+      type: Function,
     },
   },
   components: {
     TodoItem,
     TodoFilter,
-    TodoEmpty,
+    CardInfo,
   },
   setup() {
-    const todos = ref(computed(() => store.getters.fetchAllSortByCreatedAt));
-    const updateTodos = (newTodos) => {
-      todos.value = newTodos;
-    };
-
-    return { todos, updateTodos };
+    const storeTodos = computed(() => store.state.todos);
+    return { storeTodos };
   },
 };
 </script>
