0d885365c5a08d67ca7b30951f575fec426d1461
hellsontime <baber492@gmail.com>
2022-08-01 12:54:20 +0300

    implement logic to show loading

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,33 +1,48 @@
 <template>
-  <hr />
+  <div v-if="!loading">
+    <hr />
 
-  <TodoFilter :updateTodos="updateTodos" />
+    <TodoEmpty v-if="!todos.length" />
 
-  <div
-    class="w-full flex flex-col mt-6"
-    v-for="(todo, index) in todos"
-    :key="index"
-  >
-    <TodoItem :todo="todo" />
+    <div v-else>
+      <TodoFilter :updateTodos="updateTodos" />
+
+      <div
+        class="w-full flex flex-col mt-6"
+        v-for="(todo, index) in todos"
+        :key="index"
+      >
+        <TodoItem :todo="todo" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref, computed } from "vue";
+import TodoEmpty from "@/components/TodoEmpty";
 import TodoItem from "@/components/TodoList/TodoItem";
 import TodoFilter from "@/components/TodoList/TodoFilter";
 import store from "@/store";
 
 export default {
+  props: {
+    loading: {
+      required: true,
+      type: Boolean,
+    },
+  },
   components: {
     TodoItem,
     TodoFilter,
+    TodoEmpty,
   },
   setup() {
     const todos = ref(computed(() => store.getters.fetchAllSortByCreatedAt));
     const updateTodos = (newTodos) => {
       todos.value = newTodos;
     };
+
     return { todos, updateTodos };
   },
 };
