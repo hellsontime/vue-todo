5fc83e741018b370aac63e8ec3c69a67210d01d8
hellsontime <baber492@gmail.com>
2022-08-01 12:53:17 +0300

    TodoEmpty component implementation

---
+++ src/components/TodoEmpty.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col items-center justify-center my-4 mt-10">
+    <fa icon="fa-solid fa-hurricane" class="text-grey text-[48px] mb-4" />
+    <div class="capitalize text-lg text-light-black">you have no todos</div>
+    <div class="capitalize text-sm text-light-black">let's add some</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
